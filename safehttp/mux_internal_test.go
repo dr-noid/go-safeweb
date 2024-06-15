a9f91c7f0f1d010cf97ea9804a124aa8b276105e
abomapiet <timur@Timur>
2024-06-15 17:44:52 +0200

    added coverage for Server file close function, and Mux file interceptor function

---
+++ safehttp/mux_internal_test.go
@@ -0,0 +1,41 @@
+package safehttp
+
+import "testing"
+
+type MockInterceptor struct {
+}
+
+func (i *MockInterceptor) Before(w ResponseWriter, r *IncomingRequest, cfg InterceptorConfig) Result {
+    return Result{}
+}
+
+func (i *MockInterceptor) Commit(w ResponseHeadersWriter, r *IncomingRequest, resp Response, cfg InterceptorConfig) {
+}
+
+func (i *MockInterceptor) Match(InterceptorConfig) bool {
+    return true
+}
+
+type MockInterceptorConfig struct {
+}
+
+func TestInterceptorsMultipleMatches(t *testing.T) {
+    interceptors := []Interceptor{
+        &MockInterceptor{},
+    }
+
+    cfgs := []InterceptorConfig{
+        &MockInterceptorConfig{},
+        &MockInterceptorConfig{},
+        &MockInterceptorConfig{},
+    }
+
+    defer func() {
+        r := recover()
+        if r == nil {
+            t.Errorf("expected a panic")
+        }
+    }()
+
+    configureInterceptors(interceptors, cfgs)
+}
